8a9c284eeb149a76ced6d6ecfe671634fc8357a5
Dmitry Chuev <d.chuev@propellerads.net>
2021-01-06 16:49:13 +0300

    format string

--- greet.go
+++ greet.go
@@ -1,7 +1,8 @@
 package greet
 
 import "fmt"
+import "github.com/sleepy-cat/gogreet/format"
 
 func Greet(name string) string {
-	return fmt.Sprintf("Hello, %s", name)
+	return fmt.Sprintf(format.GreetingFormat(), name)
 }
